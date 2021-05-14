51ad01bc24df1bcb0c4407864bb26a85097de016
Avraz  Zebary <avrazebary@gmail.com>
2021-05-14 14:10:22 +0000

    Done.

--- db/migrate/2019_create_birds.rb
+++ db/migrate/2019_create_birds.rb
@@ -1,1 +1,32 @@
 # Add code from Readme
+ 
+class CreateBirds < ActiveRecord::Migration[5.2]
+  def change
+    create_table :birds do |t|
+      t.string :name
+      t.string :species
+ 
+      t.timestamps
+    end
+  end
+end
+
+class Bird < ApplicationRecord
+end
+
+Rails.application.routes.draw do
+  get '/birds' => 'birds#index'
+end
+
+class BirdsController < ApplicationController
+  def index
+    @birds = Bird.all
+  end
+end
+
+class BirdsController < ApplicationController
+  def index
+    @birds = Bird.all
+    render 'birds/index.html.erb'
+  end
+end
